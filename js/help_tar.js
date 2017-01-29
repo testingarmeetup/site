5419ce94536684981ecaaea68a3c40c6b5b2b2e6
arcurcio <andrescurcio@gmail.com>
2017-01-29 16:34:13 -0300

    Varios cambios. Generación dinámica de contenido desde magic.js con ES6, resultados visibles en index_es6.html. Agregado jQuery-3.1.1.min.js.

---
+++ js/help_tar.js
@@ -0,0 +1,8 @@
+  var HTMLorganizersectionStart = '<div class="col-sm-3"><div class="team-member text-center"></div></div>';
+  //var HTMLorganizersectionStyle = '';
+  var HTMLorganizersectionImgs = '<img src="imgs/"%data%" class="img-responsive img-circle">'; //img: 'imgs/curcio_andres.jpeg'
+  var HTMLorganizersectionFullName = '<h4>"%data%"</h4>'; //fullName: 'Andres Curcio'
+  var HTMLorganizersectionPosition = '<p id="position" class="text-muted">"%data%"</p>'; //job: 'Software Tester'
+  var HTMLorganizersectionSocialButtons = '<ul class="list-inline social-buttons"></ul>';
+  var HTMLorganizersectionSocialTwitter = '<li id="twitter"><a target="_blank" href="%data%"><i class="fa fa-twitter"></i></a></li>'; //twitter: 'https://twitter.com/aquintian'
+  var HTMLorganizersectionSocialLinkedin = '<li id="linkedin"><a target="_blank" href="%data%"><i class="fa fa-linkedin"></i></a></li>'; //linkedin: 'https://ar.linkedin.com/in/andrescurcio'
